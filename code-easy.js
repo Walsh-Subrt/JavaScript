fbe66c359280ea1d3662773cf6d2bf6132bc7bfc
Walsh-Subrt <josefsubrt4@gmail.com>
2025-03-25 10:20:06 +0100

    Update code-easy.js

--- code-easy.js
+++ code-easy.js
@@ -1,58 +1,65 @@
-// 1) Funkce co vrací boolean hodnotu zda arrayInput, obsahuje v sobě stringToFind
-function contains(arrayInput, stringToFind) {
-  return arrayInput.includes(stringToFind);
+// oblast definice funkcí
+ 
+// 1) Funkce co vrací boolean hodnotu zda stringInput, obsahuje v sobě stringToFind; je caseSensitive (defaultní chování)
+function contains(stringInput, stringToFind) {
+  return stringInput.includes(stringToFind)
 }
-
-// 2) Funkce co vrací délku string řetězce poskytnutého ve stringInput
-function size(stringInput) {
-  return stringInput.length;
+ 
+// 2) Funkce co vrací délku pole poskytnutého v arrayInput
+function size(arrayInput) {
+  return arrayInput.length
 }
-
-// 3) Funkce co vrací bool hodnotu co je AND mezi booleanInput1 a booleanInput2
-function andMe(booleanInput1, booleanInput2) {
-  return booleanInput1 && booleanInput2;
+ 
+// 3) Funkce co vrací opak poskytnutého boolean hodnoty v booleanInput
+function negate(booleanInput) {
+  return !booleanInput
 }
-
+ 
 // 4) Funkce co spojí (bez žádného přidaného znaku) dva textové řetězce string1 a string2 a vrátí je jako jeden string
+// například pro "Hello", "World" bude výsledek "HelloWorld"
 function join(string1, string2) {
-  return string1 + string2;
+  return `${string1}${string2}`
 }
-
+ 
 // 5) Funkce co vrací hodnotu z pole arrayInput na indexu indexInput (od 0)
+// například pro ["apple", "banana", "kiwi", "orange", "strawberry"], 2 bude výsledek "kiwi"
 function get(arrayInput, indexInput) {
-  return arrayInput[indexInput];
+  return arrayInput[indexInput]
 }
-
+ 
+ 
+ 
+ 
 // oblast volání funkcí
 const fruits = ["apple", "banana", "kiwi", "orange", "strawberry"];
-
+ 
 // 1)
-const result1 = contains(fruits, "banana");
-console.log(result1); // true
-
+const result1 = contains("Hello World", "Hello");
+console.log(result1);
+ 
 // 2)
-const result2 = size("I love learning!");
-console.log(result2); // 17
-
+const result2 = size(fruits);
+console.log(result2);
+ 
 // 3)
-const result3 = andMe(true, false);
-console.log(result3); // false
-
+const result3 = negate(true);
+console.log(result3);
+ 
 // 4)
 const result4 = join("Hello", "World");
-console.log(result4); // "HelloWorld"
-
+console.log(result4);
+ 
 // 5)
 const result5 = get(fruits, 2);
-console.log(result5); // "kiwi"
-
+console.log(result5);
+ 
 // do kódu níže nezasahujte
-
+ 
 module.exports = {
-  variant: "2A",
+  variant: "1A",
   contains,
   size,
-  andMe,
+  negate,
   join,
   get,
 };
